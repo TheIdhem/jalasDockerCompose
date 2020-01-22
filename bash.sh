6ab21a7652376fb41861ecd133ffd7ccb0a12321
mehdi <mohammadf00900@gmail.com>
2020-01-22 18:07:50 +0330

    add files that needed

---
+++ bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install docker.io -y
+
+sudo apt install git -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose up
+
